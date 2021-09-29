03ebafcaf295cd62c1960c0f296979c0f0284b27
Hkucukcayir <hkucukcayir@outlook.com>
2021-09-29 02:17:38 +0200

    Initial Commit

---
+++ Demo Real Estate App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo Real Estate App
+//
+//  Created by MacBookAir on 29/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
