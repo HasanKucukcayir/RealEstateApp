f759fd1b50a8c4fcd3c2aef43c054432a6ceb961
Hkucukcayir <hkucukcayir@outlook.com>
2021-10-26 15:19:23 +0200

    BugFix Color name

--- Demo Real Estate App/Helpers/ColorHelper.swift
+++ Demo Real Estate App/Helpers/ColorHelper.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 enum ColorHelper {
-  static let white = UIColor(named: "white")!
+  static let white = UIColor(named: "whiteCustom")!
   static let darkGray = UIColor(named: "darkGray")!
   static let lightGray = UIColor(named: "lightGray")!
   static let strong = UIColor(named: "strong")!
